d3ac98bf65baeef44253bf0bec57cc9fc95cb596
bhargavkarnati6 <159568022+bhargavkarnati6@users.noreply.github.com>
2024-03-30 00:17:49 -0300

    Add files via upload

---
+++ PasswordEncryption.java
@@ -0,0 +1,16 @@
+/*
+ This is an encryption class which encrypts the password of the users.
+ @author Bharghava Karnati
+ */
+public class PasswordEncryption {
+
+    public static String encryptPassword(String password) { // Done by Bharghav
+        char[] chars = password.toCharArray();
+        String enc = "";
+        for (char c : chars) {
+            c += 7;
+            enc += c;
+        }
+        return enc;
+    }
+}
